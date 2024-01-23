c5eb4e740abdd5508fcd89cee5b130cb7298d42a
S0ffen <whitevolf314@gmail.com>
2024-01-23 16:32:35 +0100

    ....

--- PureListOfEvents.js
+++ PureListOfEvents.js
@@ -1,3 +1,32 @@
 var Events = calendarevents;
 console.log(calendarevents);
-Events.forEach((element) => {});
+Events.forEach((element) => {
+  const formatter = new Intl.DateTimeFormat("pl-PL", {
+    day: "numeric",
+    month: "long",
+    year: "numeric",
+  });
+  var datePL = new Date(element.date[0]);
+  $("#listOfAllEvents").append(
+    `<li><span>${element.name}</span> <br> ${formatter.format(datePL)}</li>`
+  );
+});
+function Filter() {
+  var input = document.getElementById("userInput");
+  var ul = document.getElementById("listOfAllEvents");
+  var li = ul.getElementsByTagName("li");
+  var filter = input.value.toUpperCase();
+  var eventNames = calendarevents[0].name;
+  console.log(eventNames.toLowerCase());
+
+  for (i = 0; i < li.length; i++) {
+    a = li[i].getElementsByTagName("span")[0];
+    txtValue = a.textContent || a.innerText;
+    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+      console.log(txtValue.toUpperCase().indexOf(filter));
+      li[i].style.display = "";
+    } else {
+      li[i].style.display = "none";
+    }
+  }
+}
